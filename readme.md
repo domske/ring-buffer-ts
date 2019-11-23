2dda388e490d7adf93f32f07001495c1713ecd79
Dominik Geng <domske@mailbox.org>
2019-11-23 23:42:37 +0100

    Initial commit.

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Ring Buffer
+
+Aka "circular buffer" written in TypeScript
+
+- Well tested.
+- Documentation (included declaration types and doc-comments).
+- Clean and modern code.
+- Written in TypeScript (strict mode and strongly typed).
+- Easy API.
+- High performance when adding items.
+- No dependencies.
+- NodeJS, Browser.
+- Tiny size.
+
+Missing feature? Note that you can export and import the buffer as array and you can do what ever you want with the array. You can also consider to create an issue here and pull-request to add features.
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'production',
+  // mode: 'development',
+  // devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [new CleanWebpackPlugin()],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    // library: 'domske',
+    globalObject: 'this'
+  }
+};
